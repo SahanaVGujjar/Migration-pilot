dfa7b379ceb4c04571a4a195d17119cb9b1d2d9b
Shreevatsa Ganapathy Hegde <shreevatsa4321@gmail.com>
2026-05-22 22:23:00 -0700

    migration-pilot: batch 2 - migrated 3 files

---
+++ examples/generated-output/components/UserCard.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { capitalize } from '../utils/format';
+
+class UserCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+      loading: false,
+    };
+  }
+
+  toggleExpand = () => {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  };
+
+  handleDelete = () => {
+    this.setState({ loading: true });
+    if (this.props.onDelete) {
+      this.props.onDelete(this.props.user.id);
+    }
+  };
+
+  render() {
+    const { user } = this.props;
+    const { isExpanded, loading } = this.state;
+
+    return (
+      <div className={`user-card ${isExpanded ? 'expanded' : ''}`}>
+        <div className="user-card-header" onClick={this.toggleExpand}>
+          <h3>{capitalize(user.name)}</h3>
+          <span>{isExpanded ? '▼' : '▶'}</span>
+        </div>
+        {isExpanded && (
+          <div className="user-card-body">
+            <p>Email: {user.email}</p>
+            <p>Role: {user.role}</p>
+            <button onClick={this.handleDelete} disabled={loading}>
+              {loading ? 'Deleting...' : 'Delete'}
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+UserCard.propTypes = {
+  user: PropTypes.object.isRequired,
+  onDelete: PropTypes.func,
+};
+
+export default UserCard;
